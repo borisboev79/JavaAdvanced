23885df4fd064edfc9b5a3b87050d4ef99630058
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-05-30 23:41:41 +0300

    Add files via upload

---
+++ 04StreamsFiles&Directories/ADV_EXS08_GetFolderSize.java
@@ -0,0 +1,27 @@
+package Advanced_Exercise4_StreamsFilesAndDirectories;
+
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ADV_EXS08_GetFolderSize {
+    public static void main(String[] args) {
+        File folder = new File("C:\\Users\\Boevi Main\\OneDrive\\Документи\\Java\\Advanced\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\Exercises Resources");
+        Deque<File> files = new ArrayDeque<>();
+        files.offer(folder);
+        int totalSize = 0;
+        while(!files.isEmpty()){
+            File currentFile = files.poll();
+            File[] nestedFiles = currentFile.listFiles();
+            for (File nestedFile: nestedFiles) {
+                if(nestedFile.isDirectory()){
+                    files.offer(nestedFile);
+                }else{
+                    totalSize += nestedFile.length();
+                }
+            }
+        }
+        System.out.println("Folder size: "+ totalSize);
+    }
+
+}
