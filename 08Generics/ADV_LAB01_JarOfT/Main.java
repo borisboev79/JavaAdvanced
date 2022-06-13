f3e4bfe3cca7b820ab87e7c3223295953afffff1
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-06-13 23:00:04 +0300

    Add files via upload

---
+++ 08Generics/ADV_LAB01_JarOfT/Main.java
@@ -0,0 +1,15 @@
+package Advanced_Generics1_JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<Integer> jar = new Jar<>();
+
+        jar.add(13);
+        jar.add(73);
+
+        System.out.println(jar.remove());
+        System.out.println(jar.remove());
+
+    }
+
+}
